aa8764181ebf5cdf80d8c73039e4f419b40d98d4
deechean <deechean@hotmail.com>
2019-06-16 17:43:56 +0800

    compatible with tensoflow 1.10 or lower

--- tf_general.py
+++ tf_general.py
@@ -91,30 +91,41 @@ def flatten(x):
         dim *= shape[i]
     return tf.reshape(x, [-1, dim]), dim
 
-def fc_layer(x, i_size, o_size, name, is_relu=None):
+def fc_layer(x, i_size, o_size, name, activate = 'NONE'):
     """Full connection layer
         x:
         i_size: input size
         o_size: output size
         name: name of the calculation
-        is_relu: 
+        activate: RELU - relu or SIGMOID  -sigmoid, TANH - tanh
     """
     with tf.variable_scope(name) as scope:
         w = tf.get_variable('w', shape=[i_size, o_size], dtype='float')
         b = tf.get_variable('b', shape=[o_size], dtype='float')
         out = tf.nn.xw_plus_b(x, w, b, name=scope.name)
-        if is_relu:
+        
+         #activate           
+        if activate == 'SIGMOID':
+            out = tf.nn.sigmoid(out)
+        elif activate == 'TANH':
+            out = tf.nn.tanh(out)
+        elif activate == 'RELU':
             out = tf.nn.relu(out)
+    
         return out
 
-def drop_out(x, rate, name):
+def drop_out(x, drop_rate, name):
     """drop out to prevent overfit, it should only used in training, not in test
         x: input
-        keep_prob: probability of drop out, normally is 0.5
+        drop_rate: probability of drop out, normally is 0.5
         name: name of the calculation
         
     """
-    return tf.nn.dropout(x, rate=rate, name=name)
+    if tf.__version__ >= '1.13.0':
+        return tf.nn.dropout(x, rate=drop_rate, name=name)
+    else:
+        return tf.nn.dropout(x, keep_prob=1-drop_rate, name=name)
+    
     
     
 def printimages(images):
